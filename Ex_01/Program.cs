9d14c990fcc9166bc02e1dc4279c5d602e6e50e8
Svetlana <svdorohova@gmail.com>
2022-08-25 12:39:31 +0300

    work 1

---
+++ Ex_01/Program.cs
@@ -0,0 +1,12 @@
+﻿int to (int c)
+{
+    int result1 = c /10;
+    int result = result1 % 10;
+    return result;
+}
+
+
+Console.WriteLine("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = to (a);
+Console.WriteLine ($"Вторая цифра числа {result}");
