75edd1b3afa4701f86680b17ce392b6a017397b1
Svetlana <svdorohova@gmail.com>
2022-08-25 12:51:32 +0300

    work 3

---
+++ Ex_03/Program.cs
@@ -0,0 +1,64 @@
+﻿int isk (int pol)
+{
+    if (pol > 7)
+  { 
+    if (pol % 7 == 0)
+     {
+        pol = 7;
+        return pol;
+     }
+    else
+    {
+    int res1 = pol / 7;
+    int x = res1 * 7;
+    int result = pol - x;
+    return result;
+    }
+  }
+    else 
+    {
+        return pol;
+    }
+}
+
+int pok (int res)
+{
+int [] week = { 1, 2, 3, 4, 5, 6, 7 };
+int count = 6;
+int n = 0;
+while (n < count)
+{
+    if (res == week [5])
+    {
+        res = 1;
+        break;
+        
+    }
+    if (res == week [6])
+    {
+        res = 1;
+        break;
+    }
+    else 
+    {
+        res = 0;
+    }
+    n++;
+    
+}
+return res;
+}
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int res1 = isk (a);
+int result = pok (res1);
+
+
+if (result == 1)
+{
+    Console.WriteLine("Урра, выходной");
+}
+if (result == 0)
+{
+    Console.WriteLine("Еще будни");
+}
