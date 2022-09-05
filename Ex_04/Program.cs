607d03edb6a9c7595687fb0ea07d044aa506a5dc
Svetlana <svdorohova@gmail.com>
2022-09-05 10:03:24 +0300

    Work_03

--- Ex_04/Program.cs
+++ Ex_04/Program.cs
@@ -2,18 +2,11 @@
 int a = Convert.ToInt32(Console.ReadLine());
 
 int [] mass1 = new int [6];
-    mass1 [5] = a % 10;
+for (int i = 5; i >= 0; i--)
+{
+    mass1 [i] = a % 10;
     a = a / 10;
-    mass1 [4] = a % 10;
-    a = a / 10;
-    mass1 [3] = a % 10;
-    a = a / 10;
-    mass1 [2] = a % 10;
-    a = a / 10;
-    mass1 [1] = a % 10;
-    a = a / 10;
-    mass1 [0] = a;
-
+}
 if (mass1 [5] == mass1[0] && mass1[4] == mass1 [1] && mass1[3] == mass1[2])
 {
     Console.WriteLine ("Число является палиндромом");
